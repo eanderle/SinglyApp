d83f93258c95ea2a474f798f5967dd06b690d972
Dan Yang <dsyang92@gmail.com>
2012-06-02 10:52:13 -0700

    implemented callback

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import os
 from pymaps import Map, PyMap, Icon # import the libraries
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
+SINGLY_CLIENT_ID = 'b4951689e26fe3cc15c2a940db08e7b7'
+SINGLY_CLIENT_SECRET = '0e9aebaa610bd0e68cb2026934bfafbf'
+
 def getcords(d, m, s, ind):
-    # Calculate the total number of seconds, 
+    # Calculate the total number of seconds,
     # 43'41" = (43*60 + 41) = 2621 seconds.
 
     sec = float((m * 60) + s)
-    # The fractional part is total number of 
+    # The fractional part is total number of
     # seconds divided by 3600. 2621 / 3600 = ~0.728056
 
     frac = float(sec / 3600)
-    # Add fractional degrees to whole degrees 
+    # Add fractional degrees to whole degrees
     # to produce the final result: 87 + 0.728056 = 87.728056
 
     deg = float(d + frac)
@@ -73,6 +77,18 @@ def showmap():
 @app.route('/')
 def hello():
     return render_template('index.html') + showmap()
+
+@app.route('/signin')
+def toAuth():
+    url = 'https://api.singly.com/oauth/authorize?client_id=131cb07e5d121ab1fe152&redirect_uri=http://app.com/callback&service=twitter'
+
+
+@app.route('/callback')
+def toAccess():
+    code = request.args['code']
+    postdata = {'client_id': SINGLY_CLIENT_ID, 'client_secret': SINGLY_CLIENT_SECRET, 'code': code}
+    r = requests.post('https://api.singly.com/oauth/access_token', data=postdata)
+    return r.text
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
