ab172e6c85e6045f1e702bb2bd0a1648539f5265
Chris Wendel <chriwend@umich.edu>
2012-06-02 14:39:02 -0400

    added all auths...also suck a dick singly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from pymaps import Map, PyMap, Icon # import the libraries
-from flask import Flask, request
+from flask import Flask, request, redirect, session
 from flask import render_template
 import requests
 
@@ -78,17 +78,35 @@ def showmap():
 def hello():
     return render_template('index.html') + showmap()
 
-@app.route('/signin')
+@app.route('/signin-twitter')
 def toAuth():
-    url = 'https://api.singly.com/oauth/authorize?client_id=131cb07e5d121ab1fe152&redirect_uri=http://app.com/callback&service=twitter'
+    url = 'https://api.singly.com/oauth/authorize?client_id=b4951689e26fe3cc15c2a940db08e7b7&redirect_uri=http://localhost:5000/callback&service=twitter'
+    return redirect(url)
 
+@app.route('/signin-facebook')
+def authFacebook():
+    url = 'https://api.singly.com/oauth/authorize?client_id=b4951689e26fe3cc15c2a940db08e7b7&redirect_uri=http://localhost:5000/callback&service=facebook'
+    return redirect(url)
+
+@app.route('/signin-foursquare')
+def authFourSquare():
+    url = 'https://api.singly.com/oauth/authorize?client_id=b4951689e26fe3cc15c2a940db08e7b7&redirect_uri=http://localhost:5000/callback&service=foursquare'
+    return redirect(url)
+
+@app.route('/signin-instagram')
+def authInstagram():
+    url = 'https://api.singly.com/oauth/authorize?client_id=b4951689e26fe3cc15c2a940db08e7b7&redirect_uri=http://localhost:5000/callback&service=instagram'
+    return redirect(url)
 
 @app.route('/callback')
 def toAccess():
     code = request.args['code']
     postdata = {'client_id': SINGLY_CLIENT_ID, 'client_secret': SINGLY_CLIENT_SECRET, 'code': code}
     r = requests.post('https://api.singly.com/oauth/access_token', data=postdata)
-    return r.text
+    session['accesstoken'] = r.text
+    return redirect('/')
+
+app.secret_key = '\x01\xe9\xc9\xb2[\xf4l\xfc\xf0\x19\x98\xfc\x04+\xfb\x90\x14\x9f\x8e:z}\xce\t'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
